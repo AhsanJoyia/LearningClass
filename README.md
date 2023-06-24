51b88bb72605911a599e9688125670ceea121836
HM Ahsan Joyia <iamAhsanJoyia@gmail.com>
2023-06-24 16:53:00 +0500

    I have added comments to the code so that student can learn easily

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ### Which python version do I need?
 #### Flask supports Python 3.8 and newer.
-python --version
+To check python version use this command `python --version`
+
+Flask Documentation. Navigate to this link `https://flask.palletsprojects.com/en/2.3.x/`
 
 ### How to clone this project.
 use this command in terminal `git clone https://github.com/AhsanJoyia/LearningClass.git`
--- home.py
+++ home.py
@@ -2,10 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# This is our home page. When user visit our website this is the default page that will be available for the user
+
 
 @app.route("/")
 def home():
     return "Hello, Saad Zaman!"
+
+# This is about me page. When user navigate to home-page-url/about-me. home-page-url is the domain name of my website. for example google.com is domain name, same facebook.com. You can view your home-page-url when you will start your application
 
 
 @app.route("/about-me")
